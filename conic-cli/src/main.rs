34a0bcd7f1657023cada3e19e11f0f58deaa40ff
ffrancoa <ffrancoa97@gmail.com>
2025-08-03 21:31:25 -0500

    Preserve more efficient cleanup function.

--- conic-cli/src/main.rs
+++ conic-cli/src/main.rs
@@ -1,9 +1,9 @@
 use conic_core::{calc, io, CoreError};
 
 fn main() -> Result<(), CoreError> {
-    let df = io::read_csv("test/sh23-102.csv")?;
-    let null_indicators = vec![-9999.0, -8888.0, -7777.0];
-    let clean_df = calc::drop_value_rows(df, null_indicators)?;
+    let df = io::read_csv("test/sh23-104.csv")?;
+    let indicators = vec![-9999.0, -8888.0, -7777.0];
+    let clean_df = calc::filter_value_rows(df, indicators)?;
     println!("{:?}", clean_df.head(Some(5)));
     Ok(())
 }
--- conic-core/src/calc.rs
+++ conic-core/src/calc.rs
@@ -1,41 +1,12 @@
 use polars::prelude::*;
 use crate::CoreError;
 
-/// Drop rows containing NaN value indicators
-pub fn clean_nan_values(
-    raw_df: DataFrame, nan_indicators: Vec<f64>
-) -> Result<DataFrame, CoreError> {
-    // create list series for is_in() (polars ≥0.50 requirement)
-    let nan_series = Series::new(PlSmallStr::from("na_values"), nan_indicators)
-        .implode()
-        .map_err(CoreError::Polars)?
-        .into_series();
-
-    let mut exprs = Vec::new();
-    for name in raw_df.get_column_names_str() {
-        exprs.push(
-            when(col(name).is_in(lit(nan_series.clone()), false))
-                .then(lit(f64::NAN))
-                .otherwise(col(name))
-                .alias(name),
-        );
-    }
-
-    let cleaned_df = raw_df
-        .lazy()
-        .select(exprs)
-        .filter(all_horizontal([col("*").is_not_nan()])?)
-        .collect()?;
-
-    Ok(cleaned_df)
-}
-
-/// Drop rows containing any of the given value indicators
-pub fn drop_value_rows(
+/// Filter out rows containing any of the given value indicators
+pub fn filter_value_rows(
     raw_df: DataFrame,
     value_indicators: Vec<f64>,
 ) -> Result<DataFrame, CoreError> {
-    // create list series for is_in() (polars ≥0.50 requirement)
+    // create list series for is_in() (polars ≥ 0.50 requirement)
     let val_series = Series::new(PlSmallStr::from("indicators"), value_indicators)
         .implode()
         .map_err(CoreError::Polars)?
