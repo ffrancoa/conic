f835b667ab9124312721457ec3cca2135e6896d0
ffrancoa <ffrancoa97@gmail.com>
2025-08-06 21:16:16 -0500

    Improve the order of the coode (just a little bit).

--- conic-cli/src/main.rs
+++ conic-cli/src/main.rs
@@ -1,9 +1,12 @@
 use conic_core::{calc, io, CoreError};
 
 fn main() -> Result<(), CoreError> {
-    let df = io::read_csv("test/sh23-104.csv")?;
     let indicators = vec![-9999.0, -8888.0, -7777.0];
+
+    let df = io::read_csv("test/sh23-104.csv")?;
     let clean_df = calc::filter_value_rows(df, indicators)?;
-    println!("{:?}", clean_df.head(Some(5)));
+
+    println!("{:?}", clean_df.head(Some(8)));
+
     Ok(())
 }
