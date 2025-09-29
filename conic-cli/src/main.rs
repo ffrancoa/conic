1735e9888594f29d38db3a9fd84ead1b3e5917c2
ffrancoa <ffrancoa97@gmail.com>
2025-09-29 00:17:15 -0500

    Include basic calculation functions.

--- conic-cli/src/main.rs
+++ conic-cli/src/main.rs
@@ -2,11 +2,14 @@ use conic_core::{calc, io, CoreError};
 
 fn main() -> Result<(), CoreError> {
     let filter_values = vec![-9999.0, -8888.0, -7777.0];
+    let gamma = 18.7;
+    let a = 0.80;
 
     let df = io::read_csv("test/sh23-104.csv")?;
     let clean_df = calc::filter_rows(df, filter_values)?;
+    let out_df = calc::compute_basic(clean_df, a, gamma)?;
 
-    println!("{:?}", clean_df.head(Some(8)));
+    println!("{:?}", out_df.head(Some(8)));
 
     Ok(())
 }
