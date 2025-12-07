c596baa268f8319592a5f609aa89abd6b39d0bfb
ffrancoa <ffrancoa97@gmail.com>
2025-12-07 00:43:27 -0500

    Update io.rs file.

--- conic-core/src/io.rs
+++ conic-core/src/io.rs
@@ -7,32 +7,33 @@ const REQUIRED_COLUMNS: [&str; 4] = ["Depth (m)", "qc (MPa)", "fs (kPa)", "u2 (k
 
 /// Reads a CSV file into a `DataFrame`, inferring the schema.
 pub fn read_csv(file_path: &str) -> Result<DataFrame, CoreError> {
-    let mut lazy_frame = LazyCsvReader::new(PlPath::new(file_path))
-        .with_infer_schema_length(Some(0))
+    let data = CsvReadOptions::default()
+        .with_has_header(true)
+        .with_infer_schema_length(None)
+        .try_into_reader_with_file_path(Some(file_path.into()))?
         .finish()?;
-    
-    let schema = lazy_frame.collect_schema()?;
-    
-    validate_columns(&schema)?;
 
-    let raw_df = lazy_frame
-        .select(
-            schema.iter_names()
-                .map(|name| {
-                    let sname = name.as_str();
-                    col(sname)
-                        .cast(DataType::Float64)
-                        .alias(sname)
-                })
+    validate_columns(&data)?;
+
+    let schema = data.schema().clone();
+
+    let raw_data = data
+        .lazy()
+        .with_columns(
+            schema
+                .iter_names()
+                .map(|name| col(name.as_str()).cast(DataType::Float64))
                 .collect::<Vec<_>>(),
         )
         .collect()?;
 
-    Ok(raw_df)
+    Ok(raw_data)
 }
 
-/// Validates that all required columns are present in the schema.
-fn validate_columns(schema: &Arc<Schema>) -> Result<(), CoreError> {
+/// Validates that all required columns are present in the DataFrame.
+fn validate_columns(data: &DataFrame) -> Result<(), CoreError> {
+    let schema = data.schema();
+
     if let Some(missing) = REQUIRED_COLUMNS
         .iter()
         .find(|col| !schema.contains(col.as_ref()))
